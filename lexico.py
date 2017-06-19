b00d7098fc5e3e0b9c23ca512702faff66d560a3
Ludmilla Galvão <ludmillagalvao1@gmail.com>
2017-06-19 18:29:17 -0400

    Pequenos ajustes nas saídas.

--- lexico.py
+++ lexico.py
@@ -4,11 +4,8 @@ palavras_reservadas = ["if", "then", "while", "do", "write", "read", "else", "be
                        "numero_real", "program"]
 
 arquivo = open("teste.txt", "r")
-arquivo_fonte = arquivo.read()
-arquivo.seek(0, 0)
-tamanho_fonte = len(arquivo_fonte)
 
-linha = 0
+linha_atual = 0
 token = ''
 c = 0
 
@@ -21,7 +18,6 @@ def unread_char():
 def is_char(c):
     return 'a' <= c <= 'z' or 'A' <= c <= 'Z'
 
-# todo: provavelmente eu vou deletar esse c == '.'
 def is_number(c):
     return '0' <= c <= '9' or c == '.'
 
@@ -42,7 +38,7 @@ while True:
             break
 
     if read_char() == '\n':
-        linha += 1
+        linha_atual += 1
     else:
         unread_char()
 
@@ -53,13 +49,12 @@ while True:
             if is_char(c) or is_number(c):
                 token += c
             else:
-                print("Caracter inválido {} na linha {}".format(c, linha))
-
-        # todo: Adicionar token em tabela de tokens.
+                print("Caracter inválido {} na linha {}".format(repr(c), linha_atual))
+        # fixme: Adicionar token em tabela de tokens.
         if token in palavras_reservadas:
-            print('Palavra reservada: {}, Linha: {}'.format(token, linha))
+            print("Palavra reservada: '{}', linha: {}".format(token, linha_atual))
         else:
-            print('Identificador: {}, Linha: {}'.format(token, linha))
+            print("Identificador: '{}', linha: {}".format(token, linha_atual))
         token = ''
     # elif is_number(token): # Identifica se é um número (inteiro, real)
     #     c = token
@@ -84,9 +79,9 @@ while True:
     #                     if cursor > tamanho_fonte - 1:
     #                         break
     #     if real:
-    #         print(token, ": numero_real/ linha: ", linha)
+    #         print(token, ": numero_real/ linha_atual: ", linha_atual)
     #     else:
-    #         print(token, ": numero_inteiro/ linha: ", linha)
+    #         print(token, ": numero_inteiro/ linha_atual: ", linha_atual)
     #
     #     token = ''
     # ### Identifica comentários
@@ -99,9 +94,9 @@ while True:
     #         c = arquivo_fonte[cursor - 1]
     #         token += c
     #     if "}" in token:
-    #         print(token, ': comentário/ linha: ', linha)
+    #         print(token, ': comentário/ linha_atual: ', linha_atual)
     #     else:
-    #         print('erro/ linha: ', linha)
+    #         print('erro/ linha_atual: ', linha_atual)
     #     token = ''
     #     cursor += 1
     # elif token == "/":
@@ -122,10 +117,10 @@ while True:
     #                 c = arquivo_fonte[cursor - 1]
     #                 if c == "/":
     #                     token += c
-    #                     print(token, ': comentário/ linha: ', linha)
+    #                     print(token, ': comentário/ linha_atual: ', linha_atual)
     #                     flag = True
     #     else:
-    #         print(t, ": símbolo simples/ linha: ", linha)
+    #         print(t, ": símbolo simples/ linha_atual: ", linha_atual)
     #         cursor -= 1
     #     token = ''
     #     c = ''
@@ -143,9 +138,9 @@ while True:
     #             c = arquivo_fonte[cursor - 1]
     #             if c == ">" or c == "=":
     #                 token += c
-    #                 print(token, " : símbolo_duplo/ linha: ", linha)
+    #                 print(token, " : símbolo_duplo/ linha_atual: ", linha_atual)
     #             else:
-    #                 print(token, ": símbolo_simples/ linha: ", linha)
+    #                 print(token, ": símbolo_simples/ linha_atual: ", linha_atual)
     #                 cursor -= 1
     #
     #         elif c == ">" or c == ":":
@@ -155,12 +150,12 @@ while True:
     #             c = arquivo_fonte[cursor - 1]
     #             if c == "=":
     #                 token += c
-    #                 print(token, " : símbolo_duplo/ linha: ", linha)
+    #                 print(token, " : símbolo_duplo/ linha_atual: ", linha_atual)
     #             else:
-    #                 print(token, ": símbolo_simples/ linha: ", linha)
+    #                 print(token, ": símbolo_simples/ linha_atual: ", linha_atual)
     #                 cursor -= 1
     #         else:
-    #             print(token, ": simbolo simples/ linha: ", linha)
+    #             print(token, ": simbolo simples/ linha_atual: ", linha_atual)
     #         token = ''
     #         c = ''
     #         cursor += 1
