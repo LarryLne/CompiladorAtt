fa11736f343750842fa339aa586f0b90f0f83d64
Igor Breno Estácio Dutra de Oliveira <igorbreno95@outlook.com>
2017-06-25 22:41:26 -0400

    Lexer agora está agrupado em uma classe.

--- lexico.py
+++ lexico.py
@@ -1,187 +1,172 @@
 import json
 
-simbolos_simples = ["(", ")", "*", "/", "+", "-", ">", "<", "$", ";", ":", ","]
-simbolos_duplos = ["<>", ">=", "<=", ":="]
-palavras_reservadas = ["if", "then", "while", "do", "write", "read", "else",
-"begin", "end", "ident", "numero_int","procedure","numero_real", "program", "var","real","integer"]
 
-arquivo = open("teste.txt", "r")
+class Lexer():
+    def __init__(self, nome_arquivo_fonte):
+        self.simbolos_simples = ["(", ")", "*", "/", "+", "-", ">", "<", "$", ";", ":", ","]
+        self.simbolos_duplos = ["<>", ">=", "<=", ":="]
+        self.palavras_reservadas = ["if", "then", "while", "do", "write", "read", "else",
+                                    "begin", "end", "ident", "numero_int", "procedure", "numero_real", "program", "var",
+                                    "real", "integer"]
+        self.arquivo = open(nome_arquivo_fonte, "r")
+        self.linha_atual = 1  # Toma conta de que em que linha estamos no arquivo.
+        self.tabela_simbolos = []  # Dicionário para guardar a tabela de simbolos.
 
-linha_atual = 1  # Toma conta de que em que linha estamos no arquivo, não funciona 100%
-token = ''
-c = ''
+    def read_char(self):
+        return self.arquivo.read(1)
 
-tabela_simbolos = []  # Dicionário para guardar a tabela de simbolos.
+    def unread_char(self):
+        self.arquivo.seek(self.arquivo.tell() - 1, 0)
 
+    def is_char(self, c):
+        return 'a' <= c <= 'z' or 'A' <= c <= 'Z'
 
-def read_char():
-    return arquivo.read(1)
+    def is_number(self, c):
+        return '0' <= c <= '9'
 
+    def is_newline(self, c):
+        global linha_atual
+        if c == '\n':
+            self.linha_atual += 1
+            return True
+        return False
 
-def unread_char():
-    arquivo.seek(arquivo.tell() - 1, 0)
-
-
-def is_char(c):
-    return 'a' <= c <= 'z' or 'A' <= c <= 'Z'
-
-
-def is_number(c):
-    return '0' <= c <= '9'
-
-
-def is_newline(c):
-    global linha_atual
-    if c == '\n':
-        linha_atual += 1
-        return True
-    return False
-
-def reservada_ou_id (token):
-    c = token
-    while is_char(c) or is_number(c):
-        c = read_char()
-        if c == ' ' or c== '\t' : # Pula espaços.
-            continue
-        if is_char(c) or is_number(c):
-            token += c
-        else:
-            if c in simbolos_simples:
-                unread_char()
-            elif c == '':
-                continue
-            elif not is_newline(c):
-                print("Caracter inválido {} na linha {}".format(repr(c), linha_atual))
-
-    if token in palavras_reservadas:
-        tabela_simbolos.append({'Tipo': 'palavra_reservada', "Token": token, "Linha": linha_atual})
-        print("Palavra reservada: '{}', linha: {}".format(token, linha_atual))
-    else:
-        tabela_simbolos.append({'Tipo': 'identificador', "Token": token, "Linha": linha_atual})
-        print("Identificador: '{}', linha: {}".format(token, linha_atual))
-
-def inteiro_ou_real (token):
-    c = token
-    token = ''
-    real = False
-    if is_number(c):
-        while is_number(c):
-            token += c
-            c = read_char()
+    def reservada_ou_id(self, token):
+        c = token
+        while self.is_char(c) or self.is_number(c):
+            c = self.read_char()
             if c == ' ' or c == '\t':  # Pula espaços.
                 continue
-        if c == '.':
-            real = True
-            token += c
-            c = read_char()
-            while is_number(c):
+            if self.is_char(c) or self.is_number(c):
                 token += c
-                c = read_char()
+            else:
+                if c in self.simbolos_simples:
+                    self.unread_char()
+                elif c == '':
+                    continue
+                elif not self.is_newline(c):
+                    print("Caracter inválido {} na linha {}".format(repr(c), self.linha_atual))
+
+        if token in self.palavras_reservadas:
+            self.tabela_simbolos.append({'Tipo': 'palavra_reservada', "Token": token, "Linha": self.linha_atual})
+            print("Palavra reservada: '{}', linha: {}".format(token, self.linha_atual))
+        else:
+            self.tabela_simbolos.append({'Tipo': 'identificador', "Token": token, "Linha": self.linha_atual})
+            print("Identificador: '{}', linha: {}".format(token, self.linha_atual))
+
+    def inteiro_ou_real(self, token):
+        c = token
+        token = ''
+        real = False
+        if self.is_number(c):
+            while self.is_number(c):
+                token += c
+                c = self.read_char()
                 if c == ' ' or c == '\t':  # Pula espaços.
                     continue
+            if c == '.':
+                real = True
+                token += c
+                c = self.read_char()
+                while self.is_number(c):
+                    token += c
+                    c = self.read_char()
+                    if c == ' ' or c == '\t':  # Pula espaços.
+                        continue
+            else:
+                if not self.is_newline(c):
+                    print("Caracter inválido {} na linha {}".format(repr(c), linha_atual))
         else:
-            if not is_newline(c):
-                print("Caracter inválido {} na linha {}".format(repr(c), linha_atual))
-    else:
-        is_newline(c)
+            self.is_newline(c)
 
-    if real:
-        tabela_simbolos.append({'Tipo': 'numero_real', "Token": token, "Linha": linha_atual})
-        print("Numero real: '{}', linha: {}".format(token, linha_atual))
-    else:
-        tabela_simbolos.append({'Tipo': 'numero_inteiro', "Token": token, "Linha": linha_atual})
-        print("Numero inteiro: '{}', linha: {}".format(token, linha_atual))
+        if real:
+            self.tabela_simbolos.append({'Tipo': 'numero_real', "Token": token, "Linha": self.linha_atual})
+            print("Numero real: '{}', linha: {}".format(token, self.linha_atual))
+        else:
+            self.tabela_simbolos.append({'Tipo': 'numero_inteiro', "Token": token, "Linha": self.linha_atual})
+            print("Numero inteiro: '{}', linha: {}".format(token, self.linha_atual))
 
-def comentario1 (token):
-    c = token
-    while c != "}":
-        c = read_char()
-        if c == ' ' or c== '\t' : # Pula espaços.
-            continue
-        is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
-        token += c
-    if "}" in token:
-        print("Comentário: '{}', linha: {}".format(token, linha_atual))
-    else:
-        # fixme: Olhar isso no futuro.
-        print("Caracter inválido {} na linha {}".format(repr(c), linha_atual))
-    token = ''
-
-def comentario2 (token):
-    t = token
-    c = read_char()
-    is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
-    token += c
-    if c == "*":
-        flag = False
-        while flag == False:
-            c = read_char()
+    def comentario1(self, token):
+        c = token
+        while c != "}":
+            c = self.read_char()
             if c == ' ' or c == '\t':  # Pula espaços.
                 continue
-            is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
+                self.is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
             token += c
-            if c == "*":
-                c = read_char()
-                is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
-                if c == "/":
-                    token += c
-                    print("Comentário: '{}', linha: {}".format(token, linha_atual))
-                    flag = True
-
-def simbolos(token):
-    if token == "<" or token == ">" or token == ":":
-        c = read_char()
-        simb = token + c
-        if simb in simbolos_duplos:
-            token += c
-            tabela_simbolos.append({'Tipo': 'simbolo_duplo', "Token": token, "Linha": linha_atual})
-            print("Simbolo duplo: '{}', linha: {}".format(token, linha_atual))
+        if "}" in token:
+            print("Comentário: '{}', linha: {}".format(token, self.linha_atual))
         else:
-            tabela_simbolos.append({'Tipo': 'simbolo_simples', "Token": token, "Linha": linha_atual})
-            print("Simbolo simples: '{}', linha: {}".format(token, linha_atual))
-    else:
-        tabela_simbolos.append({'Tipo': 'simbolo_duplo', "Token": token, "Linha": linha_atual})
-        print("Simbolo simples: '{}', linha: {}".format(token, linha_atual))
+            # fixme: Olhar isso no futuro.
+            print("Caracter inválido {} na linha {}".format(repr(c), self.linha_atual))
+        token = ''
 
-# todo: Transformar cada etapa em funções !!!
-while True:
+    def comentario2(self, token):
+        t = token
+        c = self.read_char()
+        self.is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
+        token += c
+        if c == "*":
+            flag = False
+            while flag == False:
+                c = self.read_char()
+                if c == ' ' or c == '\t':  # Pula espaços.
+                    continue
+                    self.is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
+                token += c
+                if c == "*":
+                    c = self.read_char()
+                    self.is_newline(c)  # fixme: Não tenho certeza se é a melhor forma de checar isso.
+                    if c == "/":
+                        token += c
+                        print("Comentário: '{}', linha: {}".format(token, self.linha_atual))
+                        flag = True
 
-    if not read_char():
-        break
-    else:
-        unread_char()
+    def simbolos(self, token):
+        if token == "<" or token == ">" or token == ":":
+            c = self.read_char()
+            simb = token + c
+            if simb in self.simbolos_duplos:
+                token += c
+                self.tabela_simbolos.append({'Tipo': 'simbolo_duplo', "Token": token, "Linha": self.linha_atual})
+                print("Simbolo duplo: '{}', linha: {}".format(token, self.linha_atual))
+            else:
+                self.tabela_simbolos.append({'Tipo': 'simbolo_simples', "Token": token, "Linha": self.linha_atual})
+                print("Simbolo simples: '{}', linha: {}".format(token, self.linha_atual))
+        else:
+            self.tabela_simbolos.append({'Tipo': 'simbolo_duplo', "Token": token, "Linha": self.linha_atual})
+            print("Simbolo simples: '{}', linha: {}".format(token, self.linha_atual))
 
-    token = read_char()
-    # Lê o próximo caracter.
+    def run(self):
+        while True:
 
-    if not token:
-        continue
+            if not self.read_char():
+                break
+            else:
+                self.unread_char()
 
-    if is_newline(token):
-        continue
+            token = self.read_char()  # Lê o próximo caracter.
 
-    if token == ' ' or token == '\t':
-        continue
+            if not token:
+                continue
 
-    if is_char(token):  # Identificando se é palavra reservada ou identificador.
-        reservada_ou_id(token)
-    elif is_number(token): # Identifica se é um Numero (inteiro, real)
-        inteiro_ou_real(token)
-    elif token == "{":  # Identifica comentários
-        comentario1(token)
-    elif token == "/" and ((read_char() == "*") or unread_char()):
-        unread_char()
-        comentario2(token)
-    elif token in simbolos_simples:  # Identifica se é simbolo simples ou duplo
-        simbolos(token)
-    else:
-         print("Caracter inválido {} na linha {}".format(repr(token), linha_atual))
+            if self.is_newline(token):
+                continue
 
-# Fecha o arquivo ; )
-arquivo.close()
+            if token == ' ' or token == '\t':
+                continue
 
-# Grava a tabela em um arquivo de texto.
-tabela_simbolos_json = json.dumps(tabela_simbolos, sort_keys=True, indent=2)
-tabela = open("tabela_simbolos.txt", "w", encoding="utf-8")
-tabela.write(tabela_simbolos_json)
-tabela.close()
+            if self.is_char(token):  # Identificando se é palavra reservada ou identificador.
+                self.reservada_ou_id(token)
+            elif self.is_number(token):  # Identifica se é um Numero (inteiro, real)
+                self.inteiro_ou_real(token)
+            elif token == "{":  # Identifica comentários
+                self.comentario1(token)
+            elif token == "/" and ((self.read_char() == "*") or self.unread_char()):
+                self.unread_char()
+                self.comentario2(token)
+            elif token in self.simbolos_simples:  # Identifica se é simbolo simples ou duplo
+                self.simbolos(token)
+            else:
+                print("Caracter inválido {} na linha {}".format(repr(token), self.linha_atual))
+        return self.tabela_simbolo
